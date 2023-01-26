a50979d03f0968650246dac3b735b68b240151ec
이승제 <106712562+Lee-Seungje@users.noreply.github.com>
2023-01-26 21:54:26 +0900

    Add web socket Server

--- src/server.js
+++ src/server.js
@@ -1,4 +1,6 @@
 import express from 'express';
+import http from 'http';
+import WebSocket from 'ws';
 
 const app = express();
 app.set('view engine', 'pug');
@@ -8,4 +10,9 @@ app.get('/', (req, res) => res.render('home'));
 app.get('/*', (req, res) => res.redirect('/'));
 
 const handleListen = () => console.log('Listening on http://localhost:3000');
-app.listen(3000, handleListen);
+
+const server = http.createServer(app); // 서버 만들기(http 서버)
+
+const wss = new WebSocket.Server({ server }); // 웺 소켓 서버를 만들고 http 서버를 전달해준다 이렇게 하면 같은 서버에서 http, websocket 모두 돌릴 수 있다
+
+server.listen(3000, handleListen);
